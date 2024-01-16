4451abe2d618b589530aa93bdb59b0aef1dfe7c6
Thomas JORGE <thomas.jorge.xat@gmail.com>
2024-01-16 15:42:35 +0100

    Configuration des routes de test

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
@@ -15,5 +16,29 @@ class DefaultController extends AbstractController
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/DefaultController.php',
         ]);
+    }
+    
+    #[Route('/authentication', name: 'authentication')]
+    public function authentication(Request $request): JsonResponse
+    {
+        $result = array();
+        $username = $request->get('username');
+        $password = $request->get('password');
+        if ($username && $password) {
+            $result['username'] = $username;
+            $result['password'] = $password;
+        }
+        return $this->json($result);
+    }
+
+    #[Route('/scan', name: 'scan')]
+    public function scan(Request $request): JsonResponse
+    {
+        $result = array();
+        $data = $request->get('data');
+        if ($data) {
+            $result['data'] = $data;
+        }
+        return $this->json($result);
     }
 }
